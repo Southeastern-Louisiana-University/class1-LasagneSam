dd48de775dfd9a3e818a3814a7c3b32e93191c68
LasagneSam <samuel.dedon@selu.edu>
2025-01-23 12:16:46 -0600

    23 Jan MMXXV - fneksgnrqewjkkltjwnkgbqjhsd

---
+++ src1/reviewPart1.java
@@ -0,0 +1,27 @@
+package src1;
+
+public class reviewPart1
+{
+
+    public static void main(String[] args)
+    {
+
+        double score = 0;
+
+        if (score >= 60)
+        {
+            System.out.println("D");
+        } else if (score >= 70)
+        {
+            System.out.println("C");
+        } else if (score >= 80)
+        {
+            System.out.println("B");
+        } else if (score >= 90)
+        {
+            System.out.println("A");
+        }
+
+    }
+
+}
