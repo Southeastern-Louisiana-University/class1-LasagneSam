2a182dcc4c1bb6134e505e41131b3dea80a3bea7
LasagneSam <samuel.dedon@selu.edu>
2025-01-24 14:16:00 -0600

    24 Jan MMXXV - it gives you a grade now (probably)

--- src1/reviewPart1.java
+++ src1/reviewPart1.java
@@ -1,26 +1,48 @@
 package src1;
 
+import java.util.Scanner;
+
 public class reviewPart1
 {
 
     public static void main(String[] args)
     {
+        
+        Scanner read = new Scanner(System.in);
 
-        double score = 0;
+        System.out.print("Enter a decimal grade value: ");
+        
+        double score = read.nextDouble();
 
-
-        if (score >= 60)
+        System.out.print("Your letter grade is ");
+        
+        if (score > 100)
         {
-            System.out.println("D");
-        } else if (score >= 70)
+            System.out.print("S");
+        }
+        else if (score >= 90)
         {
-            System.out.println("C");
-        } else if (score >= 80)
+            System.out.print("A");
+        }
+        else if (score >= 80)
         {
-            System.out.println("B");
-        } else if (score >= 90)
+            System.out.print("B");
+        }
+        else if (score >= 70)
         {
-            System.out.println("A");
+            System.out.print("C");
+        }
+        else if (score >= 60)
+        {
+            System.out.print("D");
+        }
+        else if (score >= 0)
+        {
+            System.out.print("F");
+        }
+        else
+        {
+            System.out.print("X");
         }
 
     }
