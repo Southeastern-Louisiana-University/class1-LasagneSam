b66108b80a0acf098b22a9ac5f9731c7e9693bf7
LasagneSam <samuel.dedon@selu.edu>
2025-02-06 12:06:22 -0600

    6 Feb MMXXV - Account

---
+++ src1/Account.java
@@ -0,0 +1,24 @@
+package src1;
+import java.util.Scanner;
+
+public class Account
+{
+
+    private String customer;
+    private double balance;
+
+    public Account()
+    {
+
+
+
+    }
+
+    public static void main(String[] args)
+    {
+
+        Account scamuel = new Account();
+
+    }
+
+}
--- src1/ArrayReview.java
+++ src1/ArrayReview.java
@@ -1,4 +1,4 @@
-package src;
+package src1;
 
 public class ArrayReview
 {
