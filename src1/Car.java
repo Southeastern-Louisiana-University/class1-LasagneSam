2cbe949b284c2215f2c6cdad5f2be09e19905303
LasagneSam <samuel.dedon@selu.edu>
2025-02-11 12:19:02 -0600

    11 Feb MMXXV - Car class new stuff 1

--- src1/Car.java
+++ src1/Car.java
@@ -6,7 +6,8 @@ public class Car
     private String make;           // private because this will not change
     private String model;          // private because this will not change
     private int year;              // private because this will not change
-    int mileage;                   // this will change all the time
+    double mileage;                // this will change all the time
+    private int mileageDisplay;    // private because this will change with mileage BUT it is only for display
     // add three "appropriate" data fields
 
     public Car(String makeArg, String modelArg, int yearArg)
@@ -15,6 +16,7 @@ public class Car
         this.model = modelArg;
         this.year = yearArg;
         this.mileage = 0;
+        UpdateMileageDisplay(this.mileage);
     }
 
     public Car(String makeArg, String modelArg, int yearArg, int mileageArg)
@@ -23,11 +25,18 @@ public class Car
         this.model = modelArg;
         this.year = yearArg;
         this.mileage = mileageArg;
+        UpdateMileageDisplay(this.mileage);
     }
 
-    public void Drive(int milesDriven)
+    public void Drive(double milesDriven)
     {
         this.mileage += milesDriven;
+        UpdateMileageDisplay(this.mileage);
+    }
+
+    public void UpdateMileageDisplay(double newMileage)
+    {
+        this.mileageDisplay = (int) mileage;
     }
 
     // provide "appropriate" methods
